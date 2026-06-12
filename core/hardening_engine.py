0a321cc7586e3d385fedf885c252216a4b852f2e
Mahmoud <mahmoud@example.com>
2026-06-12 05:30:43 -0700

    Fixed problems and pushed

---
+++ core/hardening_engine.py
@@ -0,0 +1,233 @@
+class HardeningEngine:
+
+    def __init__(
+            self,
+            ssh_manager,
+            server_info,
+            ufw_manager,
+            fail2ban_manager,
+    ):
+
+        self.ssh = ssh_manager
+
+        self.server_info = server_info
+
+        self.ufw = ufw_manager
+
+        self.fail2ban = fail2ban_manager
+
+    def enable_firewall(
+            self,
+            extra_ports=None,
+    ):
+
+        if extra_ports is None:
+            extra_ports = []
+
+        if not self.ufw.is_installed():
+            self.ufw.install()
+
+        self.ufw.allow_ssh(
+            self.server_info.ssh_port
+        )
+
+        for port in extra_ports:
+            self.ufw.allow_port(
+                int(port)
+            )
+
+        self.ufw.enable()
+
+        self.server_info.firewall_enabled = True
+
+        self.server_info.firewall_configured = True
+
+        status = self.ufw.status()
+
+        self.server_info.firewall_status = (
+            status
+        )
+
+        return status
+
+    def disable_firewall(
+            self,
+            remove_rules: bool = False,
+    ):
+
+        self.ufw.disable()
+
+        if remove_rules:
+            self.ufw.reset()
+
+        self.server_info.firewall_enabled = False
+
+        self.server_info.firewall_configured = True
+
+        self.server_info.firewall_status = (
+            "Disabled"
+        )
+
+        return "Firewall disabled"
+
+    def install_fail2ban(self):
+
+        if not self.fail2ban.is_installed():
+            self.fail2ban.install()
+
+        self.server_info.fail2ban_installed = True
+
+        self.server_info.fail2ban_active = False
+
+        return "Fail2Ban installed"
+
+    def enable_fail2ban(self):
+
+        if not self.fail2ban.is_installed():
+            self.fail2ban.install()
+
+        self.fail2ban.configure_ssh_jail()
+
+        self.fail2ban.enable()
+
+        self.fail2ban.start()
+
+        self.fail2ban.restart()
+
+        self.server_info.fail2ban_installed = True
+
+        self.server_info.fail2ban_active = True
+
+        return "Fail2Ban enabled"
+
+    def disable_fail2ban(self):
+
+        if self.fail2ban.is_installed():
+
+            self.fail2ban.stop()
+
+            self.fail2ban.disable()
+
+        self.server_info.fail2ban_installed = True
+
+        self.server_info.fail2ban_active = False
+
+        return "Fail2Ban disabled"
+
+    def uninstall_fail2ban(self):
+
+        if self.fail2ban.is_installed():
+
+            self.fail2ban.stop()
+
+            self.fail2ban.disable()
+
+            self.fail2ban.uninstall()
+
+        self.server_info.fail2ban_installed = False
+
+        self.server_info.fail2ban_active = False
+
+        return "Fail2Ban removed"
+
+    def purge_fail2ban(self):
+
+        if self.fail2ban.is_installed():
+
+            self.fail2ban.stop()
+
+            self.fail2ban.disable()
+
+            self.fail2ban.purge()
+
+        self.server_info.fail2ban_installed = False
+
+        self.server_info.fail2ban_active = False
+
+        return "Fail2Ban purged"
+
+    def disable_root_login(self):
+
+        self.ssh.execute_sudo(
+            (
+                r"sed -i "
+                r"'s/^#\?PermitRootLogin.*/PermitRootLogin no/' "
+                r"/etc/ssh/sshd_config"
+            )
+        )
+
+        self.ssh.execute_sudo(
+            "sshd -t"
+        )
+
+        self.ssh.execute_sudo(
+            "systemctl restart ssh"
+        )
+
+        output, _, _ = (
+            self.ssh.execute_sudo(
+                "sshd -T | grep '^permitrootlogin'"
+            )
+        )
+
+        self.server_info.root_login_enabled = (
+            False
+        )
+
+        return (
+                "permitrootlogin no"
+                in output.lower()
+        )
+
+    def disable_password_login(self):
+
+        # Main sshd_config
+        self.ssh.execute_sudo(
+            (
+                r"sed -i "
+                r"'s/^#\?PasswordAuthentication.*/PasswordAuthentication no/' "
+                r"/etc/ssh/sshd_config"
+            )
+        )
+
+        # Override files (Ubuntu 24 cloud-init etc.)
+        self.ssh.execute_sudo(
+            (
+                r"find /etc/ssh/sshd_config.d "
+                r"-type f "
+                r"-exec sed -i "
+                r"'s/^#\?PasswordAuthentication.*/PasswordAuthentication no/' "
+                r"{} \;"
+            )
+        )
+
+        output, error, exit_code = (
+            self.ssh.execute_sudo(
+                "sshd -t"
+            )
+        )
+
+        if exit_code != 0:
+            return False
+
+        self.ssh.execute_sudo(
+            "systemctl restart ssh"
+        )
+
+        output, _, _ = (
+            self.ssh.execute_sudo(
+                "sshd -T | grep '^passwordauthentication'"
+            )
+        )
+
+        success = (
+                "passwordauthentication no"
+                in output.lower()
+        )
+
+        if success:
+            self.server_info.password_login_enabled = (
+                False
+            )
+
+        return success
--- hardening/hardening_manager.py
+++ hardening/hardening_manager.py
@@ -2,6 +2,10 @@ from hardening.ufw_manager import UFWManager
 from hardening.fail2ban_manager import Fail2BanManager
 from core.restore_manager import RestoreManager
 from core.backup_manager import BackupManager
+from core.hardening_engine import (
+    HardeningEngine
+)
+
 
 class HardeningManager:
 
@@ -16,6 +20,13 @@ class HardeningManager:
             ssh_manager
         )
 
+        self.engine = HardeningEngine(
+            ssh_manager=ssh_manager,
+            server_info=server_info,
+            ufw_manager=self.ufw,
+            fail2ban_manager=self.fail2ban,
+        )
+
         self.backup_manager = BackupManager(ssh_manager)
 
         self.restore = RestoreManager(
@@ -28,61 +39,22 @@ class HardeningManager:
     # -------------------------
 
     def enable_firewall(
-        self,
-        extra_ports=None,
+            self,
+            extra_ports=None,
     ):
 
-        if extra_ports is None:
-            extra_ports = []
-
-        if not self.ufw.is_installed():
-            self.ufw.install()
-
-        # Always keep SSH accessible
-        self.ufw.allow_ssh(
-            self.server_info.ssh_port
+        return self.engine.enable_firewall(
+            extra_ports=extra_ports
         )
-
-        for port in extra_ports:
-
-            self.ufw.allow_port(
-                int(port)
-            )
-
-        self.ufw.enable()
-
-        self.server_info.firewall_enabled = True
-
-        self.server_info.firewall_configured = True
-
-        status = self.ufw.status()
-
-        self.server_info.firewall_status = (
-            status
-        )
-
-        return status
 
     def disable_firewall(
-        self,
-        remove_rules: bool = False,
+            self,
+            remove_rules: bool = False,
     ):
 
-        self.ufw.disable()
-
-        if remove_rules:
-
-            self.ufw.reset()
-
-        self.server_info.firewall_enabled = False
-
-        self.server_info.firewall_configured = True
-
-        self.server_info.firewall_status = (
-            "Disabled"
+        return self.engine.disable_firewall(
+            remove_rules=remove_rules
         )
-
-        return "Firewall disabled"
 
     # -------------------------
     # Fail2Ban
@@ -90,166 +62,29 @@ class HardeningManager:
 
     def install_fail2ban(self):
 
-        if not self.fail2ban.is_installed():
-
-            self.fail2ban.install()
-
-        self.server_info.fail2ban_installed = True
-
-        self.server_info.fail2ban_active = False
-
-        return "Fail2Ban installed"
+        return self.engine.install_fail2ban()
 
     def enable_fail2ban(self):
 
-        if not self.fail2ban.is_installed():
-            self.fail2ban.install()
-
-        self.fail2ban.configure_ssh_jail()
-
-        self.fail2ban.enable()
-
-        self.fail2ban.start()
-
-        self.fail2ban.restart()
-
-        self.server_info.fail2ban_installed = True
-
-        self.server_info.fail2ban_active = True
-
-        return "Fail2Ban enabled"
+        return self.engine.enable_fail2ban()
 
     def disable_fail2ban(self):
 
-        if self.fail2ban.is_installed():
-
-            self.fail2ban.stop()
-
-            self.fail2ban.disable()
-
-        self.server_info.fail2ban_installed = True
-
-        self.server_info.fail2ban_active = False
-
-        return "Fail2Ban disabled"
+        return self.engine.disable_fail2ban()
 
     def uninstall_fail2ban(self):
 
-        if self.fail2ban.is_installed():
-
-            self.fail2ban.stop()
-
-            self.fail2ban.disable()
-
-            self.fail2ban.uninstall()
-
-        self.server_info.fail2ban_installed = False
-
-        self.server_info.fail2ban_active = False
-
-        return "Fail2Ban removed"
+        return self.engine.uninstall_fail2ban()
 
     def purge_fail2ban(self):
 
-        if self.fail2ban.is_installed():
-
-            self.fail2ban.stop()
-
-            self.fail2ban.disable()
-
-            self.fail2ban.purge()
-
-        self.server_info.fail2ban_installed = False
-
-        self.server_info.fail2ban_active = False
-
-        return "Fail2Ban purged"
+        return self.engine.purge_fail2ban()
 
     def disable_root_login(self):
-
-        self.ssh.execute_sudo(
-            (
-                r"sed -i "
-                r"'s/^#\?PermitRootLogin.*/PermitRootLogin no/' "
-                r"/etc/ssh/sshd_config"
-            )
-        )
-
-        self.ssh.execute_sudo(
-            "sshd -t"
-        )
-
-        self.ssh.execute_sudo(
-            "systemctl restart ssh"
-        )
-
-        output, _, _ = (
-            self.ssh.execute_sudo(
-                "sshd -T | grep '^permitrootlogin'"
-            )
-        )
-
-        self.server_info.root_login_enabled = (
-            False
-        )
-
-        return (
-                "permitrootlogin no"
-                in output.lower()
-        )
+        return self.engine.disable_root_login()
 
     def disable_password_login(self):
-
-        # Main sshd_config
-        self.ssh.execute_sudo(
-            (
-                r"sed -i "
-                r"'s/^#\?PasswordAuthentication.*/PasswordAuthentication no/' "
-                r"/etc/ssh/sshd_config"
-            )
-        )
-
-        # Override files (Ubuntu 24 cloud-init etc.)
-        self.ssh.execute_sudo(
-            (
-                r"find /etc/ssh/sshd_config.d "
-                r"-type f "
-                r"-exec sed -i "
-                r"'s/^#\?PasswordAuthentication.*/PasswordAuthentication no/' "
-                r"{} \;"
-            )
-        )
-
-        output, error, exit_code = (
-            self.ssh.execute_sudo(
-                "sshd -t"
-            )
-        )
-
-        if exit_code != 0:
-            return False
-
-        self.ssh.execute_sudo(
-            "systemctl restart ssh"
-        )
-
-        output, _, _ = (
-            self.ssh.execute_sudo(
-                "sshd -T | grep '^passwordauthentication'"
-            )
-        )
-
-        success = (
-                "passwordauthentication no"
-                in output.lower()
-        )
-
-        if success:
-            self.server_info.password_login_enabled = (
-                False
-            )
-
-        return success
+        return self.engine.disable_password_login()
 
     # -------------------------
     # Restore System
